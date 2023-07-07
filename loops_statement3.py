2ac93d2f6fc54b0de5dad7ed9dcf0a773c4a1e88
yougesh <58135313+yougeshdulal@users.noreply.github.com>
2023-07-07 23:38:08 +0545

    Add files via upload

---
+++ loops_statement3.py
@@ -0,0 +1,65 @@
+#sunday...........................................................
+# i = int(input("enter the number of student:"))
+#
+# a = 1
+#
+# while a <= i:
+#
+#      mark1 = float(input("enter the mark1 obtained:"))
+#      mark2 = float(input("enter the mark2 obtained:"))
+#
+#      total = mark1+mark2
+#      percentage = total/2
+#
+#      print("percentage obtained:", percentage)
+#
+#      if mark1>32 & mark2>32 & percentage >= 32:
+#         print("passeD!!!")
+#      else:
+#         print("failed!!")
+#      a = a + 1
+
+#table...........................................................
+
+# i = 1
+# num = int(input("write the number table:::"))
+#
+# while i <= 10:
+#     print(num,"*", i ,"=", num*i)
+#     i = i + 1
+
+#...nested while...............................................................
+# j = 1
+# while j < 10:
+#     print("the table of::", j)
+#     i=1
+#     while i<10:
+#         print(j,"*",i,"=",j*i)
+#         i = i + 1
+#     j=j+1
+
+#Tuesday may30....(for loop)..............................
+# result = 0
+# for i in range(1,6):
+#     result += i
+# print("result is :",result)
+
+#..........................................................
+
+# name = input("your name is :")
+# num = int(input("enter the number for factorial:"))
+# result = 1
+# for i in range(1,num+1):
+#     result = result * i
+# print(f"Factorial of {num} asked by {name} is {result}")
+
+#...........................................................
+
+
+
+
+
+
+
+
+
