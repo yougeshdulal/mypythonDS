d1eac9bdc80869007401c3c8e88b0c4100d70920
yougesh <58135313+yougeshdulal@users.noreply.github.com>
2023-07-07 23:37:43 +0545

    Add files via upload

---
+++ conditional_statement2.py
@@ -0,0 +1,66 @@
+#friday session.......................................................
+
+# a = int(input("enter the any number::"))
+# if a != 0:
+#     if a % 5 == 0:
+#         print("it is even number!!")
+#     else:
+#         print("it is odd number!!")
+# else:
+#     print("it is zero")
+
+# .....................................................................
+
+# a = True
+# print(a)
+# print(type(a))
+#
+# #............................................................
+# #
+# a = 15
+# print(a)
+# a <= 20
+#
+# while a:
+#     print("hello")
+#     a = a + 1
+
+#...............................................................
+
+# a = 1
+#
+# while a <= 6:
+#     print("hello", a)
+#     a = a + 1
+# print("out",a)
+
+#..............................................................
+
+# a = int(input("enter the starting number:"))
+# num = int(input("enter the ending number:"))
+# incerement_value = int(input("enter the increasing number:"))
+#
+# while a <= num:
+#     print(a)
+#     a = a + incerement_value
+
+#..............................................................
+
+
+# a = int(input("enter the starting number:"))
+# b = int(input("enter the ending number:"))
+#
+# if a > b:
+#     print("a is greater!!!!")
+# elif a == b:
+#     print(" they are equal")
+# else:
+#     print("b is greater!!!!")
+
+#sunday week2.................................................................
+
+
+
+
+
+
