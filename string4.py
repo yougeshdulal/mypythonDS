d46a2d219fd9643dee14ada58cc16b4c2a65a660
yougesh <58135313+yougeshdulal@users.noreply.github.com>
2023-07-07 23:38:42 +0545

    Add files via upload

---
+++ string4.py
@@ -0,0 +1,87 @@
+import random
+
+#string..............................................
+
+# name = 'yougesh '
+#
+# name1 = 'from ktm'
+# print(name[0])
+#
+# a = len(name) #length function for string length
+#
+# print(f"string length is {a}")
+# print(name.title())          #title function shows capital letter of begining as a title.
+#
+# print(name.upper())
+#
+# print(name.capitalize())
+#
+# print(name.count('o'))
+#
+# print(name[0::2])
+#
+# print(name[::-1])
+#
+# print(name[-2])
+#
+# print(name + name1)
+#
+# print(name * 2)
+
+#guess number..........................................................
+
+# guess = random.randint(1,5)
+# print(guess)
+
+#name matching.......................................................
+#
+# name1 = input("enter the name1:")
+# name2 = input("enter the name2:")
+#
+# print(f"{name1}  {name2}")
+#
+# len1 = len(name1)
+# len2 = len(name2)
+#
+# love_score = int()
+# love_score2 = int()
+#
+# average = int()
+#
+# if len1 == len2:
+#     love_score = 100
+# else:
+#     diff = len1 - len2
+#     diff = abs(diff)
+#     if diff <= 3:
+#         love_score = 50
+#     else:
+#         love_score = 80
+#
+# print("current score:",love_score)
+#
+# first_char = name1[0]
+# second_char = name2[0]
+#
+# if first_char == second_char:
+#     love_score2 = 100
+# else:
+#     love_score2 = 50
+#
+#
+# average = (love_score + love_score2)/2
+#
+# print("last lovescore is :",average)
+#
+#
+
+#june8 string class...............................................
+
+# str1 = "name"
+# str2 = 'name'
+# str3 = '''name'''         #string
+#
+# com = r'\\hello\\hi//hey'  #raw string........
+# print(com)
+
+#.................................................................
