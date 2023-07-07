810c9e414d30a5cad69b7505aacec727487e6575
yougesh <58135313+yougeshdulal@users.noreply.github.com>
2023-07-07 23:37:11 +0545

    Add files via upload

---
+++ Datatypes1.py
@@ -0,0 +1,130 @@
+# print("hello world")
+# num1 = 3
+# num2 = 26
+# num3 = num1 + num2
+# print(num3)
+
+
+#movies --  tickets,popcorn,drinks............................
+
+# ticket = 350
+# popcorn = 200
+# drinks = 200
+#
+# total = ticket+popcorn+drinks
+#
+# print("the total expense is ", ticket+popcorn+drinks)
+# print(total)
+#
+# payment = 2000
+#
+#
+#
+# ret = payment - total
+#
+# print("the return amount is " , ret)
+
+
+
+
+#house expense............................................
+
+# pani = 1500
+# batti = 250
+# rent = 15000
+# food = 5000
+# total_exp = pani + batti + rent + food
+# yearly_exp = total_exp * 12
+#
+# mon_income = 30000
+# total_icome = mon_income * 12
+#
+# print("total expense yearly  ", yearly_exp )
+#
+# print("youurs total income", total_icome)
+#
+# saving = total_icome - yearly_exp
+#
+# print("yours yearly saving", saving)
+
+#taking input..............................................
+
+# num1 = int(input("enter the number:"))
+# print("first number is : ", num1)
+#
+# num2 = int(input("enter the second number:"))
+# num3 = int(input("enter the third number:"))
+# print("2nd no. is =", num2)
+#
+# res1 = int(num1) + int(num2) +int(num3)
+#
+# print(res1)
+
+#age.........................................................
+
+# age = int(input("enter your age:"))
+# print("ageis ::",age)
+#
+# if age>=18:
+#     print("eligible for voting")
+#     if age>=22:
+#         print("eligible for bihe")
+# if age<18:
+#     print("not eligible")
+
+# day4.......................................................
+
+# shirt_p = int(input("enter the amount of shirt::"))
+# pant_p = int(input("enter the amount of pant::"))
+# shoes_p = int(input("enter the amount of shoes::"))
+#
+# total = shirt_p+pant_p+shoes_p
+#
+# print("total amount is :", total)
+#
+# payment = int(input("enter the payment amount::"))
+#
+# ret = payment - total
+#
+# if total > 0.5 * payment:
+#     print("paisa pugena aije thapnus rupes:", total - payment)
+# if payment > total:
+#     print("lignus tapai ko return amount:", ret)
+# if payment < 0.5 * total:
+#     print("majak nagarnu na,,paisa pugena aije thapnus rupes:", total - payment)
+# else:
+#     print("thanks for the payment!!!")
+
+#age...................................................................
+
+# age = int(input("enter the age:"))
+# print("your age:",age)
+#
+# if age >= 0 & age <= 100:
+#
+#     if age >= 18:
+#         print("you are eligible for voting!!")
+#     else:
+#         print("you are not eligible for voting!!")
+#
+#
+# else:
+#     print("invalid input")
+
+#strings..................................................................
+
+# fname,lname,address = input("enter your first name,last name, adress\n:").split()
+# # lname = input("enter your last name:")
+# # address = input("enter your address:")
+#
+# print(fname,lname,"lives in ", address)
+
+#..............................................................................
+
+# a = int(input("enter the any number::"))
+#
+# if a % 2 == 0:
+#     print("it is even number!!")
+# else:
+#     print("it is odd number!!")
+
